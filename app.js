0d5d445f855a821c484da5eeeb883164cb6e06a8
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-10-22 10:58:19 +0300

    subtitle

--- app.js
+++ app.js
@@ -4,37 +4,29 @@ const rezult = 5 * 43;
 const topSecionData = {
   title: 'The band!',
   subTitle: 'We love music',
+  para: 'We have created a fictional band website. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  cardName: 'Shop',
+  cardImage: 'cart.jpg',
 };
 
 const App = (
   <div className=''>
     <section className='top container'>
       <h1 className='title title--main'>{topSecionData.title}</h1>
-      <h6 className='title title--sub'>We love music</h6>
-      <p className='text--main'>
-        We have created a fictional band website. Lorem ipsum dolor sit amet,
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-        dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-        proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-        et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      </p>
+      <h6 className='title title--sub'>{topSecionData.subTitle}</h6>
+      <p className='text--main'>{topSecionData.para}</p>
       <div className='people-container d-flex justify-around'>
         <article className='people-card'>
-          <p>Name</p>
-          <img src='img/cart.jpg' alt='cart' />
+          <p>{topSecionData.cardName}</p>
+          <img src={`img/${topSecionData.cardImage}`} alt='cart' />
         </article>
         <article className='people-card'>
-          <p>Name</p>
-          <img src='img/cart.jpg' alt='cart' />
+          <p>{topSecionData.cardName}</p>
+          <img src={`img/${topSecionData.cardImage}`} alt='cart' />
         </article>
         <article className='people-card'>
-          <p>Name</p>
-          <img src='img/cart.jpg' alt='cart' />
+          <p>{topSecionData.cardName}</p>
+          <img src={`img/${topSecionData.cardImage}`} alt='cart' />
         </article>
       </div>
     </section>
